38240c71922aa7a3407fab9283b936c93c41615e
giacomobg <pepperroni@hotmail.co.uk>
2023-10-04 19:11:12 +0200

    Fix path bug

--- wacz-lightbox/vite.config.js
+++ wacz-lightbox/vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   // when compiling for production, this is useful to make sure the links to index files are correct
-  base: "https://giacomobg.github.io/wacz-component/dist",
+  base: "https://giacomobg.github.io/starling-web-ui/wacz-component/dist",
   plugins: [
     svelte({
       include: ["src/**/*.svelte"],
